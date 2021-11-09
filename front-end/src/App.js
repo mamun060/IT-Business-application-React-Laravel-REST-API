c59fe8b5b5cf49a3af2dde7c1e979e1bc9ea1bad
mamun060 <almamun.raju520@gmail.com>
2021-11-09 21:05:18 +0600

    initially done Front-End React side 5 pages

---
+++ front-end/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import HomePage from "./pages/HomePage";
+import PageTop from "./components/PageTop/PageTop";
+import TopNavigation from "./components/TopNavigation/TopNavigation";
+import AboutDescription from "./components/AboutDescription/AboutDescription";
+import AboutPage from "./pages/AboutPage";
+import CoursesPage from "./pages/CoursesPage";
+import PortfolioPage from "./pages/PortfolioPage";
+import ContactSection from "./components/ContactSection/ContactSection";
+import ContactPage from "./pages/ContactPage";
+import Services from "./components/Services/Services";
+import ServicePage from "./pages/ServicePage";
+function App() {
+  return (
+    <div>
+            <ServicePage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ front-end/src/components/TopNavigation/TopNavigation.js
@@ -0,0 +1,53 @@
+/* eslint-disable no-undef */
+import React, {Component,Fragment} from 'react';
+import {Nav, Navbar, NavDropdown} from "react-bootstrap";
+import whiteLogo from '../../asset/image/navlogo.svg'
+import blueLogo from '../../asset/image/navlogoScroll.svg'
+import '../../asset/css/custom.css'
+import '../../asset/css/bootstrap.min.css'
+class TopNavigation extends Component {
+    constructor(){
+        super();
+        this.state={
+            navBarTitle:"navTitle",
+            navBarLogo:[whiteLogo],
+            navVariant:"dark",
+            navBarBack:"navBackground",
+            navBarItem:"navItem"
+        }
+    }
+    onScroll=()=>{
+        if(window.scrollY>100){
+            this.setState({navVariant:'light',navBarTitle:'navTitleScroll', navBarLogo:[blueLogo], navBarBack:'navBackgroundScroll',navBarItem:'navItemScroll'})
+        }
+        else if(window.scrollY<100){
+            this.setState({navVariant:'dark' ,navBarTitle:'navTitle',navBarLogo:[whiteLogo],navBarBack:'navBackground',navBarItem:'navItem'})
+        }
+    }
+    componentDidMount() {
+        window.addEventListener('scroll',this.onScroll)
+    }
+    render() {
+        return (
+            <Fragment>
+                <Navbar variant={this.state.navVariant} className={this.state.navBarBack} fixed="top" collapseOnSelect expand="lg">
+                    <Navbar.Brand className={this.state.navBarTitle}><img src={this.state.navBarLogo}/>  Mamun</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="mr-auto">
+                        </Nav>
+                        <Nav>
+                            <Nav.Link className={this.state.navBarItem} href="#deets">HOME</Nav.Link>
+                            <Nav.Link className={this.state.navBarItem} href="#deets">SERVICES</Nav.Link>
+                            <Nav.Link className={this.state.navBarItem} href="#deets">COURSES</Nav.Link>
+                            <Nav.Link className={this.state.navBarItem} href="#deets">PORTFOLIO</Nav.Link>
+                            <Nav.Link className={this.state.navBarItem} href="#deets">CONTACT</Nav.Link>
+                            <Nav.Link className={this.state.navBarItem} href="#deets">ABOUT</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            </Fragment>
+        );
+    }
+}
+export default TopNavigation;
